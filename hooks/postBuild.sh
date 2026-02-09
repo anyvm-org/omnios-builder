c581f737ed1cfb6f3b3b408c56ff18260260d9e4
neil <gitpc@neilpang.com>
2026-02-09 20:07:02 +0800

    add 9.9.9.9

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -31,6 +31,6 @@ svcadm restart autofs
 rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-
+echo "nameserver 9.9.9.9" >> /etc/resolv.conf
 
 
